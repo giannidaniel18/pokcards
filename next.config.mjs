bafbdb88c78d5d02a4a89f61d9cfb5dc9ff4cee0
Daniel Gianni <giannidaniel92@gmail.com>
2024-10-27 20:29:17 -0300

    modify client id env

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: { GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID },
   reactStrictMode: false,
   images: {
     remotePatterns: [
