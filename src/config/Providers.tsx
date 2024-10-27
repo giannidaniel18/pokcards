91ac174386420924c09989360016ec123df24b25
Daniel Gianni <giannidaniel92@gmail.com>
2024-10-27 20:18:57 -0300

    modify client id env

--- src/config/Providers.tsx
+++ src/config/Providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "./theme-provider";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string;
+  const clientId = process.env.GOOGLE_CLIENT_ID as string;
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
       <GoogleOAuthProvider clientId={clientId}>{children}</GoogleOAuthProvider>
